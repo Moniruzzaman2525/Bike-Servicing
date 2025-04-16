f98663e2512afb82366156459a8affa2afd38558
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-04-16 22:33:45 +0600

    catch async and send response done

--- src/app/modules/Customer/customer.controller.ts
+++ src/app/modules/Customer/customer.controller.ts
@@ -1,8 +1,18 @@
+import catchAsync from "../../shared/catchAsync";
+import sendResponse from "../../shared/sendResponse";
+import { CustomerServices } from "./customer.services"
+import httpStatus from "http-status";
 
-
-const createCustomer = async () => {
-
-}
+const createCustomer = catchAsync(async (req, res) => {
+    const customerData = req.body
+    const result = await CustomerServices.createCustomer(customerData);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Admin fetched successfully",
+        data: result
+    })
+})
 
 
 export const CustomerController = {
